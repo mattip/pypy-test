41bc7daa40885b8ca0562f94245a4545c897ee42
mattip <matti.picus@gmail.com>
2020-04-05 13:11:10 +0300

    add py3.6, otool output

--- runtests.sh
+++ runtests.sh
@@ -5,17 +5,47 @@ set -ex
 # Download and try to pip install some packages
 
 
-URL=http://buildbot.pypy.org/nightly/trunk
+PLAT=$(python -c "import sys;print(sys.platform)")
 EXT="${PYPY##*.}"
 
-echo $EXT
-wget ${URL}/$PYPY
-if [ "$EXT" == "zip" ]; then
-    unzip $PYPY
-else
-    tar -xf $PYPY
-fi
-rm $PYPY
-cd pypy-c-*
-bin/pypy -mensurepip
-bin/pypy -mpip install flask
+for BRANCH in trunk py3.6; do
+    URL=http://buildbot.pypy.org/nightly/$BRANCH
+
+    echo
+    echo xxxxxxxxxxxxxxxx download xxxxxxxxxxxxxxxx 
+    echo
+    wget ${URL}/$PYPY
+    if [ "$EXT" == "zip" ]; then
+        unzip $PYPY
+    else
+        tar -xf $PYPY
+    fi
+    rm $PYPY
+    pushd pypy-c-*
+    if [ "$BRANCH" == "trunk" ]; then
+        BASE=pypy
+    else
+        BASE=pypy3
+    fi
+    if [ "$PLAT" == "win32" ]; then
+        BIN=./$BASE.exe
+    else
+        BIN=./bin/$BASE
+    fi
+    if [ "$PLAT" == "darwin" ]; then
+        echo
+        echo xxxxxxxxxxxxxxxx check sdk xxxxxxxxxxxxxxxx 
+        echo
+        otool -l lib_pypy/_pypy_openssl*.so 
+    fi
+    echo
+    echo xxxxxxxxxxxxxxxx ensurepip xxxxxxxxxxxxxxxx 
+    echo
+    $BIN -mensurepip
+    $BIN -mpip install flask
+    popd
+    echo
+    echo xxxxxxxxxxxxxxxx cleanup xxxxxxxxxxxxxxxx 
+    echo
+    rm -rf pypy-c-*
+done
